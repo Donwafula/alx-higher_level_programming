7d614180d80f2036d9b7e35be4c7bb17e6d12fdf
Donwafula <donwafula4@gmail.com>
2023-10-09 17:42:47 +0300

    Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,53 @@
 #include "lists.h"
 
-/**
- * reverse_linked_list - Reverses a linked list in place.
- * @head: Pointer to the head of the linked list.
- * Return: Pointer to the new head of the reversed list.
- */
-listint_t *reverse_linked_list(listint_t **head)
-{
-	listint_t *prev = NULL, *current = *head, *next = NULL;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the linked list.
- * Return: 1 if it is a palindrome, 0 otherwise.
+ * is_palindrome - function in C that checks if a singly linked
+ *                 list is a palindrome.
+ * @head: This is the input single linked list
+ * Return: Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+	listint_t *aux = *head, *current = *head;
 
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-	listint_t *second_half = NULL;
-	/*Find the middle of the linked list using slow and fast pointers*/
-	while (fast != NULL && fast->next != NULL)
+	if (*head == NULL)
+		return (1);
+	if ((*head)->next == NULL)
+		return (1);
+	while (current != NULL && aux != NULL && aux->next != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		current = current->next;
+		aux = aux->next->next;
 	}
-	/*Reverse the second half of the linked list*/
-	second_half = reverse_linked_list(&slow);
-	/*Compare the first and second halves for palindrome*/
-	while (second_half != NULL)
+	current = reverse_listint(&current);
+	aux = *head;
+	while (aux != NULL && current != NULL)
 	{
-		if ((*head)->n != second_half->n)
+		if (aux->n != current->n)
 			return (0);
-		*head = (*head)->next;
-		second_half = second_half->next;
+		aux = aux->next;
+		current = current->next;
 	}
 	return (1);
+}
+/**
+ * reverse_listint - Function that reverses a listint_t linked list
+ * @head: This is the input single linked list
+ * Return: Pointer to the first node of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = *head, *current = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = current;
+		current = *head;
+		*head = next;
+	}
+	*head = current;
+	return (*head);
 }
