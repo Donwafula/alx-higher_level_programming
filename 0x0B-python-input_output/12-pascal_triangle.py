77d77f35ac4f70fa7877a08277283c5d688f76da
Donwafula <donwafula4@gmail.com>
2023-11-08 20:02:18 +0300

    Corrected task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-'''Module for task 14'''
+"""Function efines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    '''Calculate n triangle pascal'''
-    a = []
-    for i in range(n):
-        a.append([])
-        a[i].append(1)
+    """Pascal's Triangle of size n.
 
-        for j in range(1, i):
-            a[i].append(a[i - 1][j - 1] + a[i - 1][j])
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
 
-        if(len(a) > 1):
-            a[i].append(1)
-
-    return a
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
