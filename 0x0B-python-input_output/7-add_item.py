a293e3831ce4d1a9745f32ecedd9f100cf3713e5
Donwafula <donwafula4@gmail.com>
2023-11-08 20:27:05 +0300

    Task 7a

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-'''Module for task 9'''
+'''
+script that adds all arguments to a Python list,
+and then save them to a file
+'''
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
+my_list = []
 try:
-    data = load_from_json_file('add_item.json')
-except:
-    data = []
+    tmp_list = load_file("add_item.json")
+    my_list = tmp_list
+except Exception as e:
+    pass
 
-save_to_json_file(data + sys.argv[1:], 'add_item.json')
+length = len(sys.argv)
+for i in range(1, length):
+    my_list.append(sys.argv[i])
+
+my_list = save_file(my_list, "add_item.json")
